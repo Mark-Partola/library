6b5f6d90b50aa44855520643e87607162064b89a
Partola Mark <partolamark@yandex.ru>
2014-12-17 02:34:35 +0400

    selected users info and books info

--- application/models/Model_librarian.php
+++ application/models/Model_librarian.php
@@ -7,10 +7,25 @@ class Model_librarian extends Model_abstractDb {
 
 	public function getActionsById($id, $active=null) {
 
-		$sql = "SELECT *
-					FROM `lib_actions`
-					/*INNER JOIN `lib_books`*/
-					WHERE `libr_id` = :id ";
+		$sql = "SELECT	`b`.`title`,
+						`b`.`isbn`,
+						`b`.`author`,
+						`b`.`publisher`,
+						`b`.`pub_year`,
+						`b`.`description` as `book_desc`,
+						`b`.`image_preview`,
+						`u`.`fname`,
+						`u`.`lname`,
+						`u`.`patronymic`,
+						`u`.`phone`,
+						`u`.`passport`
+					FROM `lib_actions` as `a`
+					INNER JOIN `lib_books` as `b`
+						ON `a`.`book_id` = `b`.`id`
+					INNER JOIN `lib_users` as `u`
+						ON `a`.`user_id` = `u`.`id`
+
+					WHERE `a`.`libr_id` = :id ";
 
 		if($active === true) {
 			$sql .= "AND `status` = 1";
